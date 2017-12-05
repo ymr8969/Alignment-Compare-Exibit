0a36b2c6c6cd46d0f452a52c858c4c0d1465c475
ymr8969 <34248154+ymr8969@users.noreply.github.com>
2017-12-04 21:22:55 -0500

    Add files via upload
    Newest ACE file, extra code removed, copied so analyze DNA and AA sequence

--- ACE.py
+++ ACE.py
@@ -1,54 +1,47 @@
 # python modual that allows us interact with command line
 import os
 
-# user needs to download homologene file
-print("Welcome to ACE. A tool that utilizes the clustalw program and compares two dna sequences and two amino acid sequences. Fasta files are needed from homologenome.")
-#  ask user for file name???
+# Intro to users
+print("Welcome to ACE. A tool that utilizes the ClustalW program and compares two dna sequences and two amino acid "
+      "sequences. Please download files from NCBI's Homologene; program is limited to handle only 2 sequences in a file at a time."
+      "Please make sure you have ClustalW to continue")
+#  ask user for file names
 print("Please input amino acid text file: ")
 aa = raw_input()
 print("Please input dna text file")
 dna = raw_input()
-# tell user that they can only input 2 sequences at a time
 
-# types string  bellow into command line, tells clustul to take in first file, allign and output 2nd file
-# Run clustalw on the generated files from homologene that contains multiple homologous sequences
-os.system("clustalw dna -outfile=dna_align.txt") # DNA alignment
-os.system("clustalw aa -type=protein -outfile=aa_align.txt") # Protein alignment
+# Run clustalw on the retrieved files from homologene, save to output file
+dna_cmd = "clustalw %s -outfile=dna_align.txt" %(dna)
+aa_cmd = "clustalw %s -type=protein -outfile=aa_align.txt" %(aa)
+os.system(dna_cmd) # DNA alignment
+os.system(aa_cmd) # Protein alignment
 
-# Parse files (dna and aa) to obtain both sequences and store them in variables
-# Checks similiarities between two hard-coded sequences and output their percent similiarity and difference
 
-# yara Rose
-# read in file
-#ask user for file name
-#file_name = input("What is the name of your FASTA file? ")
-
-#open file with .txt extension
-#f = open(file_name + '.out', 'r')
-f = open('dna_align.txt', 'r')
+# Yara Rose
+#  File reading and parsing sequences in file to desire strings
+f = open('dna_align.txt', 'r') #open output file from ClustalW
 num_lines = 0
 # count the number of lines
 with f:
     for num_lines, l in enumerate(f):
         pass
 
-i = 0
-f = open('dna_align.txt', 'r') # file from CLUSTALW
-f.readline() # need to skip over frist 3 lines
+i = 0 # initialize counter variable
+f = open('dna_align.txt', 'r')
+f.readline() # need to skip over first 3 lines
 f.readline()
 f.readline()
-seq1 = ''
-seq2 = ''
+seq1 = '' # initialize sequence 1
+seq2 = '' # initialize sequence 2
 while i< (num_lines-3):
-    # reads a ine, and cuts out identification seqment, only saves sequence
+    # reads a line, and cuts out identification segment, only saves sequence
     line1 = (f.readline())
     split1 = line1.split('.', 1)
-    #print(split1[1])
     seq1 += split1[1]
 
     line2 = (f.readline())
     split2 = line2.split('.', 1)
-    #print(split2[1])
     seq2 += split2[1]
 
     f.readline()
@@ -58,34 +51,80 @@ while i< (num_lines-3):
 seq1 = seq1.replace(' ','')
 seq2 = seq2.replace(' ','')
 
-#print(seq1)
-
-# need to take out
-#seq1 = "MQGAFGKPQGTVARVHIGQVIMSIRTKLQNKEHVIEALRRAKFKFPGRQKIHISKKWGFTKFNADEFEDMVAEKRLIPDGCGVKYIPSRGPLDKWRALHS"
-#seq2 = "MRGAFGKPQGTVARVHIGQVIMSIRTKLQNKEHVIEALCRAKFKFPGRQKIHISKKWGFTKFNADEFEDMVAEKRLIPDGCGVKYIPNPGPLDKWRALHS"
-#seq1list =  seq1.split(' ',1)
-#seq2list =  seq2.split(' ',1)
-
 length = len(seq1)
 
-# print both hard coded sequences to user
+# print both sequences to user
 print "%s\n%s" %(seq1, seq2)
 samecount = float(0)
 diffcount = float(0)
-#print "%s" % (samecount)
 
 # compares charater at each position to check if same
 for a in range(0,length): 
-#	print "%s" % (samecount)
-	if seq1[a] == seq2[a]:
-#		print "same"
+	if seq1[a] == seq2[a] and seq1[a] != "-":
 		samecount = samecount + 1
+		
 	else:
-#		print "different"
 		diffcount = diffcount + 1
 
-#calculate percent same and different and print
+# calculate percent same and different and print
 samepercent = float(samecount) / float(length)
 diffpercent = float(diffcount) / float(length)
-print "Percent similar: %s\nPercent different: %s" %(samepercent, diffpercent)
+print "DNA percent similar: %s\nDNA percent different: %s" %(samepercent, diffpercent)
+
+
+
+# same as above but for Proteins
+
+f = open('aa_align.txt', 'r')  # open output file from ClustalW
+num_lines = 0
+# count the number of lines
+with f:
+    for num_lines, l in enumerate(f):
+        pass
+
+i = 0  # initialize counter variable
+f = open('aa_align.txt', 'r')
+f.readline()  # need to skip over first 3 lines
+f.readline()
+f.readline()
+seq1 = ''  # initialize sequence 1
+seq2 = ''  # initialize sequence 2
+while i < (num_lines - 3):
+    # reads a line, and cuts out identification segment, only saves sequence
+    line1 = (f.readline())
+    split1 = line1.split('.', 1)
+    seq1 += split1[1]
+
+    line2 = (f.readline())
+    split2 = line2.split('.', 1)
+    seq2 += split2[1]
+
+    f.readline()
+    f.readline()
+    i += 4
+
+seq1 = seq1.replace(' ', '')
+seq2 = seq2.replace(' ', '')
+
+length = len(seq1)
+
+# print both sequences to user
+print
+"%s\n%s" % (seq1, seq2)
+samecount = float(0)
+diffcount = float(0)
+
+# compares charater at each position to check if same
+for a in range(0, length):
+    if seq1[a] == seq2[a] and seq1[a] != "-":
+        samecount = samecount + 1
+
+    else:
+        diffcount = diffcount + 1
+
+# calculate percent same and different and print
+samepercent = float(samecount) / float(length)
+diffpercent = float(diffcount) / float(length)
+print
+"Amino Acid percent similar: %s\nAmino Acid percent different: %s" % (samepercent, diffpercent)
 
